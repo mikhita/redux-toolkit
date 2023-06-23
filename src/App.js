7146b6b1c1dfc786088e2fadd554bcdfa2b02ea5
mikhita <mikheilispiriani@yahoo.com>
2023-06-23 10:46:03 +0400

    Separate the creation of new anecdotes into a component called AnecdoteForm. Move all logic for creating a new anecdote into this new component. is done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { vote, createAnecdote } from './reducers/anecdoteReducer'
+import { vote } from './reducers/anecdoteReducer'
+import NewAnecdote from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
-  }
   const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes);
 
   
@@ -27,10 +22,7 @@ const App = () => {
       </div>
     ))}
       <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name="anecdote"/></div>
-        <button type='submit'>create</button>
-      </form>
+      <NewAnecdote/>
     </div>
   )
 }
